3bf5c09b922fc63438f20597b9b2be555f6eb6d7
Almanac <ruoyw0@gmail.com>
2021-02-22 13:22:15 -0800

    Onwards to Remove method. Start with cases of item does not exist in list. Check that Count property does not change

---
+++ CustomListUnitTesting/RemoveUnitTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListImplementation;
+using System;
+
+namespace CustomListUnitTesting
+{
+    [TestClass]
+    public class RemoveUnitTest
+    {
+        [TestMethod]
+        public void Remove_EmptyList_ShouldReturnFalse()
+        {
+            // Arrange
+            CustomList<int> intList = new CustomList<int>();
+
+            // Act
+            bool expected = false;
+            bool actual = intList.Remove(1);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Remove_FourIntList_ItemNotInThere_ShouldReturnFalse()
+        {
+            // Arrange
+            CustomList<int> intList = new CustomList<int>();
+
+            // Act
+            intList.Add(2);
+            intList.Add(3);
+            intList.Add(4);
+            intList.Add(5);
+            bool expected = false;
+            bool actual = intList.Remove(1);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_FourIntList_ItemNotInThere_CountShouldNotChange()
+        {
+            // Arrange
+            CustomList<int> intList = new CustomList<int>();
+
+            intList.Add(2);
+            intList.Add(3);
+            intList.Add(4);
+            intList.Add(5);
+            // Act
+            int expected = 4;
+            intList.Remove(1);
+            int actual = intList.Count;
+            
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
