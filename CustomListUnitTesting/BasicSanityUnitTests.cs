c34539bb7af91164c8b10bb2616622512de10152
Almanac <ruoyw0@gmail.com>
2021-02-22 09:06:53 -0800

    Added in test to check string generic instantiation + count starts at 0. Added in test case for capacity starts at 5

--- CustomListUnitTesting/BasicSanityUnitTests.cs
+++ CustomListUnitTesting/BasicSanityUnitTests.cs
@@ -20,5 +20,31 @@ namespace CustomListUnitTesting
             // Assert
             Assert.AreEqual(actual, expected);
         }
+        [TestMethod]
+        public void Constructor_CreateStringGeneric_CountStartsAtZero()
+        {
+            // Arrange
+            CustomList<string> cList = new CustomList<string>();
+
+            // Act
+            int expected = 0;
+            int actual = cList.Count;
+
+            // Assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void Constructor_CreateIntGeneric_CapacityStartsAtFive()
+        {
+            // Arrange
+            CustomList<int> cList = new CustomList<int>();
+
+            // Act
+            int expected = 5;
+            int actual = cList.Capacity;
+
+            // Assert
+            Assert.AreEqual(actual, expected);
+        }
     }
 }
