f037352ce5be876f71a55f51cb2366fecdbbcf3d
Almanac <ruoyw0@gmail.com>
2021-02-22 14:08:43 -0800

    Remove tests part 1. Testing basic functionalities. Just check value at index and count values for different cases. Next part will be some more tests, and also to check capacity.

--- CustomListUnitTesting/RemoveUnitTest.cs
+++ CustomListUnitTesting/RemoveUnitTest.cs
@@ -52,10 +52,109 @@ namespace CustomListUnitTesting
             int expected = 4;
             intList.Remove(1);
             int actual = intList.Count;
-            
+
 
             // Assert
             Assert.AreEqual(expected, actual);
         }
+        [TestMethod]
+        public void Remove_FourIntList_OneMatch_ShouldReturnTrue()
+        {
+            // Arrange
+            CustomList<int> intList = new CustomList<int>();
+
+            intList.Add(1);
+            intList.Add(2);
+            intList.Add(3);
+            intList.Add(4);
+            // Act
+            bool expected = false;
+            bool actual = intList.Remove(1);
+
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Remove_FourIntList_OneMatch_CountShouldDecreaseByOne()
+        {
+            // Arrange
+            CustomList<int> intList = new CustomList<int>();
+
+            intList.Add(1);
+            intList.Add(2);
+            intList.Add(3);
+            intList.Add(4);
+            // Act
+            int expected = intList.Count - 1;
+            intList.Remove(1);
+            int actual = intList.Count;
+
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Remove_FourIntList_OneMatchAtBeginning_OtherThreeValuesRemainsSame()
+        {
+            // Arrange
+            CustomList<int> intList = new CustomList<int>();
+
+            intList.Add(100);
+            intList.Add(0);
+            intList.Add(1);
+            intList.Add(2);
+            // Act
+            intList.Remove(100);
+
+            // Assert
+            for (int i = 0; i < intList.Count; i++)
+            {
+                Assert.AreEqual(i, intList[i]);
+            }
+        }
+        [TestMethod]
+        public void Remove_FourIntList_TwoMatch_CountShouldDecreaseByOne()
+        {
+            // Arrange
+            CustomList<int> intList = new CustomList<int>();
+
+            intList.Add(1);
+            intList.Add(1);
+            intList.Add(3);
+            intList.Add(4);
+            // Act
+            int expected = intList.Count - 1;
+            intList.Remove(1);
+            int actual = intList.Count;
+
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void Remove_FourIntList_OneMatchInMiddle_OtherValuesShouldRemainSame()
+        {
+            // Arrange
+            CustomList<int> intList = new CustomList<int>();
+
+            intList.Add(0);
+            intList.Add(100);
+            intList.Add(1);
+            intList.Add(2);
+            
+            // Act
+            intList.Remove(100);
+
+            // Assert
+            // Assert
+            for (int i = 0; i < intList.Count; i++)
+            {
+                Assert.AreEqual(i, intList[i]);
+            }
+        }
+
     }
 }
