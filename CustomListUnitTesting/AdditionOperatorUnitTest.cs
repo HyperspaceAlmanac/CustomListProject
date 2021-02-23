de85599e5a1b81a079166786eee21c6320db92d2
Almanac <ruoyw0@gmail.com>
2021-02-23 08:12:31 -0800

    Added in tests that checks for count. Added in test cases for 2 x 1 item CustomLists

--- CustomListUnitTesting/AdditionOperatorUnitTest.cs
+++ CustomListUnitTesting/AdditionOperatorUnitTest.cs
@@ -23,6 +23,21 @@ namespace CustomListUnitTesting
         }
 
         [TestMethod]
+        public void AdditionOverload_EmptyListAndEmptyList_CountShouldBeZero()
+        {
+            // Arrange
+            CustomList<int> left = new CustomList<int>();
+            CustomList<int> right = new CustomList<int>();
+
+            // Act
+            int expected = 0;
+            int actual = (left + right).Count;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
         public void AdditionOverload_EmptyListAndOneItem_ShouldReturnOneItem()
         {
             // Arrange
@@ -41,6 +56,22 @@ namespace CustomListUnitTesting
         }
 
         [TestMethod]
+        public void AdditionOverload_EmptyListAndOneItem_CountShouldBeOne()
+        {
+            // Arrange
+            CustomList<int> left = new CustomList<int>();
+            CustomList<int> right = new CustomList<int>();
+            right.Add(1);
+
+            // Act
+            int expected = 1;
+            int actual = (left + right).Count;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
         public void AdditionOverload_OneItemAndEmptyList_ShouldReturnOneItem()
         {
             // Arrange
@@ -53,6 +84,57 @@ namespace CustomListUnitTesting
             temp.Add(1);
             string expected = temp.ToString();
             string actual = (left + right).ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void AdditionOverload_OneItemAndEmptyList_CountShouldBeOne()
+        {
+            // Arrange
+            CustomList<int> left = new CustomList<int>();
+            CustomList<int> right = new CustomList<int>();
+            right.Add(1);
+
+            // Act
+            int expected = 1;
+            int actual = (left + right).Count;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AdditionOverload_OneItemAndOneItem_ShouldReturnTwoItems()
+        {
+            // Arrange
+            CustomList<int> left = new CustomList<int>();
+            left.Add(1);
+            CustomList<int> right = new CustomList<int>();
+            left.Add(2);
+
+            // Act
+            CustomList<int> temp = new CustomList<int>();
+            temp.Add(1);
+            temp.Add(2);
+            string expected = temp.ToString();
+            string actual = (left + right).ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void AdditionOverload_OneItemAndOneItem_CountShouldBeTwo()
+        {
+            // Arrange
+            CustomList<int> left = new CustomList<int>();
+            left.Add(1);
+            CustomList<int> right = new CustomList<int>();
+            right.Add(2);
+
+            // Act
+            int expected = 2;
+            int actual = (left + right).Count;
 
             // Assert
             Assert.AreEqual(expected, actual);
