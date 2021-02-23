a3cfb1952582b951cb9eec2232a45fdf085f40ae
Almanac <ruoyw0@gmail.com>
2021-02-23 13:57:55 -0800

    Added in Zip Unit test skeleton

--- CustomListUnitTesting/ZipUnitTest.cs
+++ CustomListUnitTesting/ZipUnitTest.cs
@@ -7,9 +7,83 @@ namespace CustomListUnitTesting
     [TestClass]
     public class ZipUnitTest
     {
+        // Double capacity of first List until everything can fit
         [TestMethod]
         public void Zip_TwoEmptyLists_ShouldReturnNewEmptyList()
         {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Zip_ThreeItem_EmptyList_ShouldReturnThreeItems()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Zip_ThreeItem_EmptyList_CapacityShouldBeFour()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Zip_ThreeItems_ThreeItems_ShouldReturnSixItems()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Zip_FiveItems_ThreeItems_ShouldReturnEightItems()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        [TestMethod]
+        public void Zip_FiveItems_ThreeItems_CapacityShouldBeEight()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void Zip_FourItems_SixItems_ShouldReturnTenItems()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+        [TestMethod]
+        public void Zip_FourItems_SixItems_CapacityShouldBeSixteen()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
         }
     }
 }
