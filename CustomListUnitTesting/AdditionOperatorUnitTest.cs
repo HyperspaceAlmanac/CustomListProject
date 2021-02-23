b9a12d850bed727a27826cbd9665b2a2a5c9a215
Almanac <ruoyw0@gmail.com>
2021-02-23 08:07:16 -0800

    Added in first three basic tests for empty + 1 item, 1 item + empty, and empty + empty

---
+++ CustomListUnitTesting/AdditionOperatorUnitTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListImplementation;
+using System;
+
+namespace CustomListUnitTesting
+{
+    [TestClass]
+    public class AdditionOperatorUnitTest
+    {
+        [TestMethod]
+        public void AdditionOverload_EmptyListAndEmptyList_ShouldReturnEmptyList()
+        {
+            // Arrange
+            CustomList<int> left = new CustomList<int>();
+            CustomList<int> right = new CustomList<int>();
+
+            // Act
+            string expected = new CustomList<int>().ToString();
+            string actual = (left + right).ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AdditionOverload_EmptyListAndOneItem_ShouldReturnOneItem()
+        {
+            // Arrange
+            CustomList<int> left = new CustomList<int>();
+            CustomList<int> right = new CustomList<int>();
+            right.Add(1);
+
+            // Act
+            CustomList<int> temp = new CustomList<int>();
+            temp.Add(1);
+            string expected = temp.ToString();
+            string actual = (left + right).ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AdditionOverload_OneItemAndEmptyList_ShouldReturnOneItem()
+        {
+            // Arrange
+            CustomList<int> left = new CustomList<int>();
+            left.Add(1);
+            CustomList<int> right = new CustomList<int>();
+
+            // Act
+            CustomList<int> temp = new CustomList<int>();
+            temp.Add(1);
+            string expected = temp.ToString();
+            string actual = (left + right).ToString();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
