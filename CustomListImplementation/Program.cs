e4d764a00de7aa5b5e665b953fdc80fa9cc70a22
Almanac <ruoyw0@gmail.com>
2021-02-23 13:21:08 -0800

    Start adding in tests for Zip method

--- CustomListImplementation/Program.cs
+++ CustomListImplementation/Program.cs
@@ -11,6 +11,6 @@ namespace CustomListImplementation
         static void Main(string[] args)
         {
 
-        }
+		}
     }
 }
---
+++ CustomListUnitTesting/ZipUnitTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListImplementation;
+using System;
+
+namespace CustomListUnitTesting
+{
+    [TestClass]
+    public class ZipUnitTest
+    {
+        [TestMethod]
+        public void Zip_TwoEmptyLists_ShouldReturnNewEmptyList()
+        {
+        }
+    }
+}
